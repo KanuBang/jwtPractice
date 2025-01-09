1333a478f01432ea3cf1b35c309248fad47599f7
gdrffg <gdrffg@gmail.com>
2025-01-09 17:31:17 +0900

    create controller

---
+++ src/main/java/com/example/jwt_tutorial/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.example.jwt_tutorial.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin controller";
+    }
+}
