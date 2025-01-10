ea1f1fb27d1e806ec728441fc01de0de650444fa
gdrffg <gdrffg@gmail.com>
2025-01-10 17:04:46 +0900

    login filter & authentification based on DB

---
+++ src/main/java/com/example/jwt_tutorial/jwt/LoginFilter.java
@@ -0,0 +1,57 @@
+package com.example.jwt_tutorial.jwt;
+
+import com.example.jwt_tutorial.dto.LoginDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Slf4j
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        /* form - data
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+         */
+
+        // json
+        ObjectMapper objectMapper = new ObjectMapper();
+        LoginDTO loginDTO = null;
+        try {
+            loginDTO = objectMapper.readValue(request.getInputStream(), LoginDTO.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String username = loginDTO.getUsername();
+        String password = loginDTO.getPassword();
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
+        System.out.println(username);
+
+        return authenticationManager.authenticate(authToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+
+    }
+}
